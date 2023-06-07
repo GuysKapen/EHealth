d7f087828f41f92d921c2cd95fef66351196ebe2
GuysKapen <guyskapen@gmail.com>
2023-06-07 20:35:55 +0700

    Add chief complaint

---
+++ client/src/views/admin/chief_complaint/EditView.vue
@@ -0,0 +1,39 @@
+<script setup>
+import ItemForm from "@/components/admin/ChiefComplaintForm.vue";
+</script>
+
+<template>
+  <div class="my-8 w-1/2 mx-auto py-16 px-8">
+    <div class="header">
+      <h2
+        class="text-2xl font-black leading-7 text-gray-900 sm:text-3xl sm:truncate"
+      >
+        Edit chief complaint
+      </h2>
+    </div>
+    <ItemForm :model="model" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+const serverUrl = import.meta.env.VITE_SERVER_URL;
+import { useAuthStore } from "@/stores/auth/auth";
+export default {
+  data: () => ({ model: null }),
+  mounted() {
+    const authStore = useAuthStore();
+    axios
+      .get(`${serverUrl}/admin/chief-complaint/${this.$route.params.id}`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${authStore.token}`,
+          "x-access-token": authStore.token,
+        },
+      })
+      .then((res) => {
+        this.model = res.data;
+      });
+  },
+};
+</script>
---
+++ client/src/views/admin/chief_complaint/NewView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ItemForm from "@/components/admin/ChiefComplaintForm.vue";
+</script>
+
+<template>
+  <div class="my-8 w-1/2 mx-auto py-16 px-8">
+    <div class="header">
+      <h2
+        class="text-2xl font-black leading-7 text-gray-900 sm:text-3xl sm:truncate"
+      >
+        Add new CC
+      </h2>
+    </div>
+    <ItemForm />
+  </div>
+</template>
